02c3023919a1a78f6525c7c4dfece14a8f330b76
GrantRT <granto_92@hotmail.com>
2022-03-03 00:00:37 +0000

    refactored functions in classes to make semantical

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,9 +1,7 @@
 function generateHtml(teamMembers) {
   const htmlString = teamMembers.reduce((string, employee) => {
-    return string + `\t<li> job role: ${employee.constructor.name}\n name: ${employee.name}\n ID: ${employee.id}\n email: ${employee.email}\n ${employee.render()}${employee.getFourthInput()}</li>\n`;
+    return string + `\t<li> job role: ${employee.constructor.name}\n name: ${employee.name}\n ID: ${employee.id}\n email: ${employee.email}\n ${employee.render()}</li>\n`;
   }, '\n');
-
-  const renderList = (list) => `<ul>${list}</ul>`;
 
   const list = renderList(htmlString);
 
@@ -11,15 +9,16 @@ function generateHtml(teamMembers) {
   return `<!DOCTYPE html>
   <html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
   </head>
-    <body>
-    ${list}
-    </body>
+  <body>
+  ${list}
+  </body>
   </html>`;
 }
+const renderList = (list) => `<ul>${list}</ul>`;
 
 module.exports = generateHtml;
